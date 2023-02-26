14f76fac1d9663f9780d82aedac05a04557354e8
xoron <rmaher84@gmail.com>
2023-02-26 16:38:38 +0000

    Deploying to gh-pages from @ sytac-webpack-module-federation/signup@4589d60e565cc50f04e1caa59e0175598bfe2ee1 🚀

---
+++ bootstrap_js.main.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunksignup"] = self["webpackChunksignup"] || []).push([["bootstrap_js"],{
+
+/***/ "./bootstrap.js":
+/*!**********************!*\
+  !*** ./bootstrap.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"webpack/sharing/consume/default/react/react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"webpack/sharing/consume/default/react-dom/react-dom\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _src_Signup__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./src/Signup */ \"./src/Signup.js\");\n\n\n\nconst App = () => {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_src_Signup__WEBPACK_IMPORTED_MODULE_2__[\"default\"], null);\n};\nreact_dom__WEBPACK_IMPORTED_MODULE_1___default().render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(App, null), document.getElementById('app'));\n\n//# sourceURL=webpack://signup/./bootstrap.js?");
+
+/***/ }),
+
+/***/ "./src/Signup.js":
+/*!***********************!*\
+  !*** ./src/Signup.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"webpack/sharing/consume/default/react/react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var uicomponents_Button__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! uicomponents/Button */ \"webpack/container/remote/uicomponents/Button\");\n/* harmony import */ var uicomponents_Button__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(uicomponents_Button__WEBPACK_IMPORTED_MODULE_1__);\n\n\nconst Signup = ({\n  children\n}) => {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h1\", null, \"Sign up now\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", null, \"Here is a basic form you can use to sign up for our service. We will never sell your data or spam you.\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((uicomponents_Button__WEBPACK_IMPORTED_MODULE_1___default()), null, \"Submit\"));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Signup);\n\n//# sourceURL=webpack://signup/./src/Signup.js?");
+
+/***/ })
+
+}]);
---
+++ src_Signup_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunksignup"] = self["webpackChunksignup"] || []).push([["src_Signup_js"],{
+
+/***/ "./src/Signup.js":
+/*!***********************!*\
+  !*** ./src/Signup.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"webpack/sharing/consume/default/react/react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var uicomponents_Button__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! uicomponents/Button */ \"webpack/container/remote/uicomponents/Button\");\n/* harmony import */ var uicomponents_Button__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(uicomponents_Button__WEBPACK_IMPORTED_MODULE_1__);\n\n\nconst Signup = ({\n  children\n}) => {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"h1\", null, \"Sign up now\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(\"p\", null, \"Here is a basic form you can use to sign up for our service. We will never sell your data or spam you.\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((uicomponents_Button__WEBPACK_IMPORTED_MODULE_1___default()), null, \"Submit\"));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Signup);\n\n//# sourceURL=webpack://signup/./src/Signup.js?");
+
+/***/ })
+
+}]);
